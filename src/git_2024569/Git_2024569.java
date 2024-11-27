95e9d438a7027c61ad35a2b5d265f868895c99b2
Iveelt-B <Iveeelt@gmail.com>
2024-11-27 22:01:30 +0000

    Added a simple print of name and student ID

--- src/git_2024569/Git_2024569.java
+++ src/git_2024569/Git_2024569.java
@@ -14,7 +14,7 @@ public class Git_2024569 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Iveelt Batjargal - 2024569");
     }
     
 }
